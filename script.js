5e6f07cf1a471d4a67698f95e6248c936360f7c8
xxIslexx <eltchi.aslambekov@gmail.com>
2024-06-21 19:19:47 +0300

    s

--- script.js
+++ script.js
@@ -74,4 +74,68 @@ $(document).ready(function(){
         document.querySelector('.overlay_modal').style.display = 'none';
         document.getElementById('modal4').style.display = 'none';
     });
+
+    //MODAL 5
+    document.getElementById('img5').addEventListener('click', function() {
+      document.querySelector('.overlay_modal').style.display = 'block';
+      document.getElementById('modal5').style.display = 'block';
+    });
+
+    document.getElementById('closeButton5').addEventListener('click', function() {
+        document.querySelector('.overlay_modal').style.display = 'none';
+        document.getElementById('modal5').style.display = 'none';
+    });
+
+    document.querySelector('.overlay_modal').addEventListener('click', function() {
+        document.querySelector('.overlay_modal').style.display = 'none';
+        document.getElementById('modal5').style.display = 'none';
+    });
+
+    //MODAL 6
+    document.getElementById('img6').addEventListener('click', function() {
+      document.querySelector('.overlay_modal').style.display = 'block';
+      document.getElementById('modal6').style.display = 'block';
+    });
+
+    document.getElementById('closeButton6').addEventListener('click', function() {
+        document.querySelector('.overlay_modal').style.display = 'none';
+        document.getElementById('modal6').style.display = 'none';
+    });
+
+    document.querySelector('.overlay_modal').addEventListener('click', function() {
+        document.querySelector('.overlay_modal').style.display = 'none';
+        document.getElementById('modal6').style.display = 'none';
+    });
+
+    //MODAL 7
+    document.getElementById('img4').addEventListener('click', function() {
+      document.querySelector('.overlay_modal').style.display = 'block';
+      document.getElementById('modal4').style.display = 'block';
+    });
+
+    document.getElementById('closeButton4').addEventListener('click', function() {
+        document.querySelector('.overlay_modal').style.display = 'none';
+        document.getElementById('modal4').style.display = 'none';
+    });
+
+    document.querySelector('.overlay_modal').addEventListener('click', function() {
+        document.querySelector('.overlay_modal').style.display = 'none';
+        document.getElementById('modal4').style.display = 'none';
+    });
+
+    //MODAL 8
+    document.getElementById('img4').addEventListener('click', function() {
+      document.querySelector('.overlay_modal').style.display = 'block';
+      document.getElementById('modal4').style.display = 'block';
+    });
+
+    document.getElementById('closeButton4').addEventListener('click', function() {
+        document.querySelector('.overlay_modal').style.display = 'none';
+        document.getElementById('modal4').style.display = 'none';
+    });
+
+    document.querySelector('.overlay_modal').addEventListener('click', function() {
+        document.querySelector('.overlay_modal').style.display = 'none';
+        document.getElementById('modal4').style.display = 'none';
+    });
   });
